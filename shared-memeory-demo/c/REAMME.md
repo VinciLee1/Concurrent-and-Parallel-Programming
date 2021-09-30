e07058be1a6ebbedba2561ac8ee90dcf88f94bb5
vinci lee <vincilee@rr1wdgtest1974.guest.corp.microsoft.com>
2021-09-30 23:43:19 +0800

    shared memroy through ipc

---
+++ shared-memeory-demo/c/REAMME.md
@@ -0,0 +1,5 @@
+compile and execute putMem, this will create a shared memroy address,
+and then copy the string "Hello, World!" to that memroy address.
+An infinite loop will start until the first char of that shared memory address is not 'H'.
+
+compile and execute editMem, you will chage the first char of the above shared memroy address, so the putMem process will exit.  
---
+++ shared-memeory-demo/c/editMem.c
@@ -0,0 +1,41 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void report_and_die(const char * msg) {
+  perror(msg);
+  exit(-1);
+}
+
+char * setup(key_t key, int byte_count, int flags){
+  /* Get the shared memory and its identifier */
+  int mem_id = shmget(key,
+                      byte_count,
+                      flags);
+  if (mem_id < 0) report_and_die("failed  to shmget");
+  /* attach memory to putter's address space */
+  char * mem_ptr = shmat(mem_id,
+                         0,
+                         0);
+  if (mem_ptr == (void*) -1 ) report_and_die("failed on shmat");
+  return mem_ptr;
+}
+
+int main() {
+  const char * greeting = "Hello, world!";
+  int len = strlen(greeting) + 1;
+  key_t key = 9876;
+  
+  char * mem_ptr = setup(key, len,  0666);
+  
+  *mem_ptr = 'h';
+  
+  puts(mem_ptr);
+
+  return 0;
+}
+
